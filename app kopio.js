4ea106649d7618ddb25d8be9d32bb56e230708de
siirihytonen <100684138+siirihytonen@users.noreply.github.com>
2022-05-10 14:33:11 +0300

    Tiedoston siirto
    siirto

---
+++ app kopio.js
@@ -0,0 +1,53 @@
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar_menu');
+
+
+const mobileMenu = () =>{
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+}
+
+menu.addEventListener('click',mobileMenu);
+
+
+ 
+const highlightMenu = () => {
+    const elem=document.querySelector('.highlight');
+    const homeMenu=document.querySelector('#home-page');
+    const aboutMenu=document.querySelector('#about-page');
+    const projectMenu=document.querySelector('#project-page');
+    let scrollPos = window.scrollY;
+
+    if(window.innerWidth>960 && scrollPos < 600){
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+        return
+    }   else if (window.innerWidth > 960 && scrollPos < 1400){
+        aboutMenu.classList.add('highlight');
+        homeMenu.classList.remove('highlight');
+        projectMenu.classList.remove('highlight');
+        return
+    }   else if(window.innerWidth > 960 && scrollPos < 2345){
+        aboutMenu.classList.remove('highlight');
+        projectMenu.classList.add('highlight');
+        return
+    }
+
+    if((elem && window.innerWidth < 960 && scrollPos < 600) || elem){
+        elem.classList.remove('highlight');
+    }
+}
+
+window.addEventListener('scroll',highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+const hideMobileMenu =() => {
+    const menuBars = document.querySelector('is-active');
+    if(window.innerWidth <=768 && menuBars){
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+
+    }
+
+menuLinks.addEventListener('click',hideMobileMenu);
